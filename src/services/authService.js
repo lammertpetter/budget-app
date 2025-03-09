ef54eaf5057468683955a57ad957b3ac2a789dee
lammert <Petterlammert@gmail.com>
2025-03-09 00:44:05 +0000

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://budget-9ks45pjtz-lammerts-projects.vercel.app/';
+const API_URL = 'https://budget-9ks45pjtz-lammerts-projects.vercel.app/api/auth';
 
 export const registerUser = async (userData) => {
     try {
