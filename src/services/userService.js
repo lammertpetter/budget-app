08ee6479f0cc885d0133478e7a2f0cf1b786d5bf
lammert <Petterlammert@gmail.com>
2025-03-09 00:43:26 +0000

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://budget-9ks45pjtz-lammerts-projects.vercel.app/';
+const API_URL = 'https://budget-9ks45pjtz-lammerts-projects.vercel.app/api/user';
 
 export const fetchUserData = async (token) => {
     try {
